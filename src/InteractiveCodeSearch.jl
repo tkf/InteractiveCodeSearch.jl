c8dbed03dcecd29c5963dd4519a95f5f6f354e2d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-18 20:09:31 -0700

    Add simple @search macro

--- src/InteractiveCodeSearch.jl
+++ src/InteractiveCodeSearch.jl
@@ -1,5 +1,35 @@
 module InteractiveCodeSearch
+export @search
 
-# package code goes here
+function read_stdout(cmd, input)
+    stdout, stdin, process = readandwrite(cmd)
+    reader = @async read(stdout)
+    write(stdin, input)
+    close(stdin)
+    return wait(reader)
+end
+
+function parse_loc(line)
+    rest, lineno = rsplit(line, ":", limit=2)
+    _, path = rsplit(rest, " at ", limit=2)
+    return String(path), parse(Int, lineno)
+end
+
+function code_search(methods::Base.MethodList)
+    out = String(read_stdout(`peco`, join(map(string, methods), "\n")))
+    path, lineno = parse_loc(out)
+    info("Opening $path:$lineno")
+    edit(path, lineno)
+end
+
+code_search(f::Base.Callable) = code_search(methods(f))
+
+macro search(x)
+    if x isa Symbol
+        :(code_search($(esc(x))))
+    else
+        :(@edit $(esc(x)))
+    end
+end
 
 end # module
