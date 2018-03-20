c1700f4da91cf52243c7ec4474f9aacd8720c7db
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-19 21:15:39 -0700

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
-using InteractiveCodeSearch
+module TestInteractiveCodeSearch
+
+using InteractiveCodeSearch: read_stdout, parse_loc
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "read_stdout" begin
+    @test strip(String(read_stdout(`cat`, "spam"))) == "spam"
+end
+
+@testset "parse_loc" begin
+    @test parse_loc("@test(ex::ANY, kws...) in Base.Test at test.jl:249") ==
+        ("test.jl", 249)
+end
+
+end  # module
