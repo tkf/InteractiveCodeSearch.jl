8a6ad14d390466b3a63b816912c0ae8e8110c6ff
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-30 17:17:10 -0700

    Refactor code to make it testable

--- src/InteractiveCodeSearch.jl
+++ src/InteractiveCodeSearch.jl
@@ -1,6 +1,11 @@
 module InteractiveCodeSearch
 export @search
 
+mutable struct SearchConfig  # CONFIG
+    open
+    interactive_matcher
+end
+
 is_identifier(s) = ismatch(r"^@?[a-z_]+$"i, string(s))
 
 is_locatables(::Any) = false
@@ -42,20 +47,40 @@ function parse_loc(line)
     return String(path), parse(Int, lineno)
 end
 
-function search_methods(methods)
-    out = String(read_stdout(`peco`, join(map(string, methods), "\n")))
+run_matcher(input) = String(read_stdout(CONFIG.interactive_matcher, input))
+
+function choose_method(methods)
+    out = run_matcher(join(map(string, methods), "\n"))
     if isempty(out)
         return
     end
-    path, lineno = parse_loc(out)
+    return parse_loc(out)
+end
+
+function default_openline(path, lineno)
     info("Opening $path:$lineno")
     edit(path, lineno)
 end
+
+maybe_open(::Void) = nothing
+maybe_open(x::Tuple{String, Int}) = CONFIG.open(x...)
+
+search_methods(methods) = maybe_open(choose_method(methods))
 
 
 code_search(f::Base.Callable) = code_search(methods(f))
 code_search(ms::Base.MethodList) = search_methods(ms)
 code_search(m::Module) = search_methods(module_methods(m))
+
+function code_search(x::T) where T
+    warn("Cannot search for $x; searching for its type $T instead...")
+    code_search(T)
+end
+
+const CONFIG = SearchConfig(
+    default_openline,           # open
+    `peco`,                     # interactive_matcher
+)
 
 macro search(x)
     if x isa Symbol || x isa Expr && x.head == :.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,18 @@
 module TestInteractiveCodeSearch
 
-using InteractiveCodeSearch: read_stdout, parse_loc
+using InteractiveCodeSearch
+using InteractiveCodeSearch: list_locatables, module_methods,
+    read_stdout, parse_loc
 using Base.Test
+
+macro test_nothrow(ex)
+    quote
+        @test begin
+            $(esc(ex))
+            true
+        end
+    end
+end
 
 @testset "read_stdout" begin
     @test strip(String(read_stdout(`cat`, "spam"))) == "spam"
@@ -10,6 +21,48 @@ end
 @testset "parse_loc" begin
     @test parse_loc("@test(ex::ANY, kws...) in Base.Test at test.jl:249") ==
         ("test.jl", 249)
+end
+
+@testset "list_locatables" begin
+    locs = Set(list_locatables(InteractiveCodeSearch))
+    @test Set([
+        InteractiveCodeSearch.list_locatables,
+        InteractiveCodeSearch.module_methods,
+        InteractiveCodeSearch.read_stdout,
+        InteractiveCodeSearch.parse_loc,
+        InteractiveCodeSearch.search_methods,
+        getfield(InteractiveCodeSearch, Symbol("@search")),
+    ]) <= locs
+end
+
+@testset "module_methods" begin
+    @test_nothrow module_methods(InteractiveCodeSearch)
+    @test_nothrow module_methods(Base.Filesystem)
+end
+
+@testset "patched" begin
+    config = deepcopy(InteractiveCodeSearch.CONFIG)
+    try
+        open_args = []
+        dummy_openline(args...) = push!(open_args, args)
+
+        let c = InteractiveCodeSearch.CONFIG
+            c.interactive_matcher = `echo " at test.jl:249"`
+            c.open = dummy_openline
+        end
+
+        @test_nothrow @eval @search read_stdout
+        @test_nothrow @eval @search @search
+        @test_nothrow @eval @search InteractiveCodeSearch
+        @test open_args == repeat([("test.jl", 249)], outer=3)
+
+        # @show open_args
+    finally
+        let c = InteractiveCodeSearch.CONFIG
+            c.interactive_matcher = config.interactive_matcher
+            c.open = config.open
+        end
+    end
 end
 
 end  # module
