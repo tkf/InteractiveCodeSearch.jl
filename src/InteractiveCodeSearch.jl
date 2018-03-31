a14f6e538e89b0785c752f0b23bcd15d3c52e343
Takafumi Arakaki <aka.tkf@gmail.com>
2018-03-30 17:14:05 -0700

    Make Module searchable

--- src/InteractiveCodeSearch.jl
+++ src/InteractiveCodeSearch.jl
@@ -1,6 +1,33 @@
 module InteractiveCodeSearch
 export @search
 
+is_identifier(s) = ismatch(r"^@?[a-z_]+$"i, string(s))
+
+is_locatables(::Any) = false
+is_locatables(::Base.Callable) = true
+
+function list_locatables(m::Module)
+    locs = []
+    for s in names(m, true)
+        if is_identifier(s)
+            x = try
+                getfield(m, s)
+            catch err
+                err isa UndefVarError && continue
+                rethrow()
+            end
+            if is_locatables(x)
+                push!(locs, x)
+            end
+        end
+    end
+    return locs
+end
+
+module_methods(m::Module) :: Vector{Method} =
+    vcat(collect.(methods.(list_locatables(m)))...)
+
+
 function read_stdout(cmd, input)
     stdout, stdin, process = readandwrite(cmd)
     reader = @async read(stdout)
@@ -15,7 +42,7 @@ function parse_loc(line)
     return String(path), parse(Int, lineno)
 end
 
-function code_search(methods::Base.MethodList)
+function search_methods(methods)
     out = String(read_stdout(`peco`, join(map(string, methods), "\n")))
     if isempty(out)
         return
@@ -25,7 +52,10 @@ function code_search(methods::Base.MethodList)
     edit(path, lineno)
 end
 
+
 code_search(f::Base.Callable) = code_search(methods(f))
+code_search(ms::Base.MethodList) = search_methods(ms)
+code_search(m::Module) = search_methods(module_methods(m))
 
 macro search(x)
     if x isa Symbol || x isa Expr && x.head == :.
