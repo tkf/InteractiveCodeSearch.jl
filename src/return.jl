65c7695d327ee93376fb6513406808718b73fc4f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-10-20 12:07:19 -0700

    Support canceling background search

--- src/return.jl
+++ src/return.jl
@@ -3,6 +3,7 @@ export @searchreturn
 struct CallableFinder{P <: SearchPolicy}
     modules::AbstractVector{Module}
     sp::P
+    should_stop::Base.RefValue{Bool}
 end
 
 Base.IteratorSize(::Type{<: CallableFinder}) = Base.SizeUnknown()
@@ -52,6 +53,9 @@ function Base.iterate(cf::CallableFinder, state::CFState)
     for i in state.i_name:lastindex(state.names)
         name = state.names[i]
         yield()  # Maybe throttle? Or Julia scheduler would do it?
+        if cf.should_stop[]
+            return nothing
+        end
         if isdefined(m, name)
             x = getproperty(m, name)
             if (cf.sp isa Recursive &&
@@ -94,12 +98,13 @@ rettype_is(typ::Type) = method -> rettype_is(method, typ)
 
 function search_by_rettype(sp::SearchPolicy,
                            typ::Type,
-                           modules::AbstractVector{Module})
+                           modules::AbstractVector{Module},
+                           should_stop::Ref{Bool} = Ref(false))
     if isempty(modules)
         modules = Base.loaded_modules_array()
     end
-    # methiters = map(methods, CallableFinder(modules, sp))  # -> array
-    methiters = Base.Generator(methods, CallableFinder(modules, sp))
+    callables = CallableFinder(modules, sp, should_stop)
+    methiters = Base.Generator(methods, callables)
     meths = Iterators.flatten(methiters)
     return Iterators.filter(rettype_is(typ), meths)
 end
@@ -115,7 +120,8 @@ struct BackgroundSearch
     query::SearchQuery
     found::Vector
     task::Task
-    done::Ref{Bool}
+    done::Base.RefValue{Bool}
+    should_stop::Base.RefValue{Bool}
 end
 
 function showquery(io::IO, query::SearchQuery)
@@ -142,14 +148,21 @@ end
 function Base.show(io::IO, search::BackgroundSearch)
     print(io, nameof(typeof(search)), " id=", search.id)
 
-    # " [done]" or " [active]"
+    # print " [done]" etc.
     print(io, " [")
-    if search.done[]
+    if search.should_stop[]
+        printstyled(io, "canceled"; color=:yellow)
+    elseif search.done[]
         printstyled(io, "done"; color=:green)
+    elseif istaskdone(search.task)
+        printstyled(io, "error"; color=:light_red, bold=true)
     else
         printstyled(io, "active"; color=:red)
     end
     print(io, "]")
+    # Notes: `istaskdone(search.task)` can't be used here since `show`
+    # is called at the end of the `search.task`.  (Though this can be
+    # worked around by using additional task watching `search.task`.)
 
     print(io, " ", length(search.found), " found")
     return
@@ -158,7 +171,7 @@ end
 function Base.show(io::IO, ::MIME"text/plain", search::BackgroundSearch)
     show(io, search)
     println(io)
-    if search.done[]
+    if search.done[] || search.should_stop[] || istaskdone(search.task)
         print(io, "Searched ")
     else
         print(io, "Searching ")
@@ -171,15 +184,16 @@ function _start_search_return(id, args...)
     query = SearchQuery(args...)
     found = []
     done = Ref(false)
+    should_stop = Ref(false)
     task = @async begin
-        append!(found, search_by_rettype(args...))
+        append!(found, search_by_rettype(args..., should_stop))
         done[] = true
 
         @info "Finished search id=$id.  Look it up by `@search $id`."
         show(stderr, "text/plain", background_searches[id])
         println(stderr)
     end
-    return BackgroundSearch(id, query, found, task, done)
+    return BackgroundSearch(id, query, found, task, done, should_stop)
 end
 
 const background_searches = BackgroundSearch[]
@@ -195,19 +209,50 @@ function schedule_search_return(typ::Type,
     return search
 end
 
+"""
+    kill(search::InteractiveCodeSearch.BackgroundSearch)
+
+Stop `search`.
+"""
+Base.kill(search::BackgroundSearch) = stop(search)
+
+"""
+    InteractiveCodeSearch.stop(search::BackgroundSearch)
+    InteractiveCodeSearch.stop(id::Int = 0)
+
+Stop `search`.  The first form can be invoked via `kill(search)` as well
+(where `kill` is the function exported from `Base`).  See also `bg`.
+"""
+function stop(search::BackgroundSearch)
+    # schedule(...; error=true) is not the right way to stop the task?
+    # https://github.com/JuliaLang/julia/issues/25353#issuecomment-354807622
+    search.should_stop[] = true
+    wait(search.task)
+    return
+end
+stop(id::Int = 0) = kill(bg(id))
+
+"""
+    InteractiveCodeSearch.bg(id::Int = 0)
+
+Lookup background search by `id`.  For convenience, `id = 0` is the
+last search and `id = -1` is the second last search.  More generally,
+non-positive `id` is treated as the offset from `end`.
+"""
+function bg(id::Int = 0)
+    if id < 1
+        return background_searches[end + id]
+    else
+        return background_searches[id]
+    end
+end
+
 # @search _s1
 code_search(::SearchPolicy, search::BackgroundSearch) =
     search_methods(search.found)
 
 # @search 1
-function code_search(p::SearchPolicy, id::Int)
-    if id < 1
-        search = background_searches[end + id]
-    else
-        search = background_searches[id]
-    end
-    return code_search(p, search)
-end
+code_search(p::SearchPolicy, id::Int) = code_search(p, bg(id))
 
 function searchreturn(typ::Type, modules::AbstractVector{Module};
                       policy = Recursive())
@@ -221,6 +266,7 @@ Search functions returning type `Type` in `Module`s.  As this search
 typically takes some time to finish, interactive matcher will not be
 launched by this command.  Instead, a "handle" to the search in
 background is returned which can be queried via `@search` later.
+Calling `kill` (`Base.kill`) on the handle cancels the search.
 
 # Limitations
 
@@ -246,6 +292,8 @@ BackgroundSearch id=1 [active] 0 found
 Searching ::AbstractArray{T,1} where T from Module[LinearAlgebra SparseArrays] recursively
 
 julia> @search _s1
+
+julia> kill(_s1)  # stop the search
 ```
 
 If you prefer giving a custom name to the search result, just assign it to
